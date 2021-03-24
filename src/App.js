ac6f87f26a2320b26af065b6ae17df72fd2929d8
orow1993 <orow1993@gmail.com>
2021-03-24 12:37:11 +0200

    local db

--- src/App.js
+++ src/App.js
@@ -1,21 +1,40 @@
 import logo from './logo.svg';
 import './App.css';
 import React from "react";
+import data from "./data"
 
-function App() {
-  return (
-    <div className="grid-container">
-        <header>
-            <a href="/">TipTop Shop</a>
-        </header>
-        <main>
-           Products List
-        </main>
-        <footer>
-            Footer
-        </footer>
-    </div>
-  );
+class App extends React.Component {
+    constructor() {
+        super();
+        this.state={
+            products:data.products,
+            size:"",
+            sort:"",
+        };
+    }
+    render() {
+        return (
+            <div className="grid-container">
+                <header>
+                    <a href="/">TipTop Shop</a>
+                </header>
+                <main>
+                    <div className="content">
+                        <div className="main">Products</div>
+                        <div className="sidebar">Cart Items</div>
+                    </div>
+                </main>
+                <footer>
+                    Footer
+                </footer>
+            </div>
+        );
+    }
+
+}
+
+{
+
 }
 
 export default App;
